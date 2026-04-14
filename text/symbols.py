cfc57a388b23c31bfc03d657e1cecd2de5be3785
fatimaharoon1 <158079284+fatimaharoon1@users.noreply.github.com>
2026-04-14 13:41:39 +0500

    Update symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -7,23 +7,14 @@ The default is a set of ASCII characters that works well for English or text tha
 
 from text import cmudict, pinyin
 
-_pad = "_"
-_punctuation = "!'(),.:;? "
+__pad = "_"
+_punctuation = "،۔!؟ "
 _special = "-"
-_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-_silences = ["@sp", "@spn", "@sil"]
+_letters = "اآبپتٹثجچحخدڈذرڑزژسشصضطظعغفقکگلمنوہھءیے"
 
-# Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
-_arpabet = ["@" + s for s in cmudict.valid_symbols]
-_pinyin = ["@" + s for s in pinyin.valid_symbols]
-
-# Export all symbols:
 symbols = (
     [_pad]
     + list(_special)
     + list(_punctuation)
     + list(_letters)
-    + _arpabet
-    + _pinyin
-    + _silences
 )

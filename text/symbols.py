73479ebac8e27342d353aba6e1fb32851e19d4ae
fatimaharoon1 <158079284+fatimaharoon1@users.noreply.github.com>
2026-04-14 14:00:52 +0500

    Update symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -5,16 +5,24 @@ Defines the set of symbols used in text input to the model.
 
 The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. """
 
-from text import cmudict, pinyin
+# from https://github.com/keithito/tacotron
+
+"""
+Defines symbols for Urdu FastSpeech2 training.
+"""
 
 __pad = "_"
-_punctuation = "،۔!؟ "
 _special = "-"
+_punctuation = "،۔!؟ "
 _letters = "اآبپتٹثجچحخدڈذرڑزژسشصضطظعغفقکگلمنوہھءیے"
 
+# (optional but safe)
+_silences = ["@sp", "@sil"]
+
 symbols = (
-    [_pad]
+    [__pad]
     + list(_special)
     + list(_punctuation)
     + list(_letters)
+    + _silences
 )
